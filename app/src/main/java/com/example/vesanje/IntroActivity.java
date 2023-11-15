3577f168418d3c5df96c4c3a2f87185667fb6788
Jovan Bogdanovic <bogdanovicjob@gmail.com>
2023-11-15 20:40:56 +0100

    Added double-back to exit app

--- app/src/main/java/com/example/vesanje/IntroActivity.java
+++ app/src/main/java/com/example/vesanje/IntroActivity.java
@@ -2,15 +2,20 @@ package com.example.vesanje;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IntroActivity extends AppCompatActivity {
 
+    private boolean doubleBackToExitPressedOnce = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
@@ -53,5 +58,16 @@ public class IntroActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("category", category);
         startActivity(intent);
+    }
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            finishAffinity();
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000); // Change the delay as needed
     }
 }
