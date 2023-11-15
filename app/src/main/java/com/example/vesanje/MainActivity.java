69c8467be66807da7806641ec7dc99be7bde96e8
Jovan Bogdanovic <bogdanovicjob@gmail.com>
2023-11-15 17:14:05 +0100

    First commit
    Simple Hangman game app.

---
+++ app/src/main/java/com/example/vesanje/MainActivity.java
@@ -0,0 +1,146 @@
+package com.example.vesanje;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String secretWord = "hangman";
+    private StringBuilder displayWord = new StringBuilder("_______");
+    private int remainingGuesses = 6;
+
+    private TextView wordTextView;
+    private TextView EndTextView;
+    private TextView EndWordTextView;
+    private TextView guessesTextView;
+    private GridLayout keyboardGrid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wordTextView = findViewById(R.id.wordTextView);
+        EndTextView = findViewById(R.id.EndTextView);
+        EndWordTextView = findViewById(R.id.EndWordTextView);
+        guessesTextView = findViewById(R.id.guessesTextView);
+        keyboardGrid = findViewById(R.id.keyboardGrid);
+
+        updateWordDisplay();
+        displayGuesses();
+
+        setupKeyboard();
+    }
+
+    private void setupKeyboard() {
+        for (int i = 0; i < keyboardGrid.getChildCount(); i++) {
+            View child = keyboardGrid.getChildAt(i);
+
+            if (child instanceof TextView) {
+                final TextView textView = (TextView) child;
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        handleGuess(textView.getText().toString());
+                        textView.setEnabled(false);
+                    }
+                });
+            }
+        }
+    }
+
+    private void handleGuess(String guess) {
+        boolean letterGuessed = false;
+        guess = guess.toUpperCase();
+
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (Character.toUpperCase(secretWord.charAt(i)) == guess.charAt(0)) {
+                displayWord.setCharAt(i, secretWord.charAt(i));
+                letterGuessed = true;
+            }
+        }
+
+        if (!letterGuessed) {
+            remainingGuesses--;
+        }
+
+        displayGuesses();
+        updateWordDisplay();
+        checkGameStatus();
+    }
+
+    private void updateWordDisplay() {
+        wordTextView.setText(displayWord.toString());
+    }
+
+    private void displayGuesses() {
+        guessesTextView.setText("Guesses left: " + remainingGuesses);
+    }
+
+    private void checkGameStatus() {
+        if (displayWord.toString().equals(secretWord)) {
+            // Player wins
+            EndTextView.setText("Congratulations!");
+            EndWordTextView.setText("You guessed the word: " + secretWord);
+            disableKeyboard();
+            showTryAgainButton();
+        } else if (remainingGuesses == 0) {
+            // Player loses
+            EndTextView.setText("Sorry, you ran out of guesses.");
+            EndWordTextView.setText("The word was: " + secretWord);
+            disableKeyboard();
+            showTryAgainButton();
+        }
+    }
+
+    private void showTryAgainButton() {
+        Button tryAgainButton = findViewById(R.id.playAgainButton);
+        tryAgainButton.setVisibility(View.VISIBLE);
+        tryAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Reset the game when "Try Again" is clicked
+                resetGame();
+            }
+        });
+    }
+
+    private void resetGame() {
+        secretWord = "hangman";
+        displayWord = new StringBuilder("_______");
+        remainingGuesses = 6;
+        EndTextView.setText("");
+        EndWordTextView.setText("");
+
+        updateWordDisplay();
+        displayGuesses();
+
+        Button tryAgainButton = findViewById(R.id.playAgainButton);
+        tryAgainButton.setVisibility(View.GONE);
+
+        enableKeyboard();
+    }
+
+    private void enableKeyboard() {
+        for (int i = 0; i < keyboardGrid.getChildCount(); i++) {
+            View child = keyboardGrid.getChildAt(i);
+            if (child instanceof TextView) {
+                child.setEnabled(true);
+            }
+        }
+    }
+
+    private void disableKeyboard() {
+        for (int i = 0; i < keyboardGrid.getChildCount(); i++) {
+            View child = keyboardGrid.getChildAt(i);
+            if (child instanceof TextView) {
+                child.setEnabled(false);
+            }
+        }
+    }
+}
