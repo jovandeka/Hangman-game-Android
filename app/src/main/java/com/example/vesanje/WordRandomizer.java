fd9546d2723866e33946e7de0a6525a515903fe8
Jovan Bogdanovic <bogdanovicjob@gmail.com>
2023-11-25 00:53:23 +0100

    WordRandomizer class is now dynamic & added category "person"

--- app/src/main/java/com/example/vesanje/WordRandomizer.java
+++ app/src/main/java/com/example/vesanje/WordRandomizer.java
@@ -32,49 +32,15 @@ public class WordRandomizer {
 
     private static ArrayInfo getRandomArrayInfo(Context context) {
         Resources resources = context.getResources();
-        String[] fruitArray = resources.getStringArray(R.array.fruit);
-        String[] animalArray = resources.getStringArray(R.array.animal);
-        String[] colorArray = resources.getStringArray(R.array.color);
-        String[] instrumentArray = resources.getStringArray(R.array.instrument);
-        String[] sportArray = resources.getStringArray(R.array.sport);
-        String[] footballArray = resources.getStringArray(R.array.club);
-        String[] countryArray = resources.getStringArray(R.array.country);
-        String[] foodArray = resources.getStringArray(R.array.food);
-        String[] vegetableArray = resources.getStringArray(R.array.vegetable);
-        String[] movieArray = resources.getStringArray(R.array.movie);
-        String[] brandArray = resources.getStringArray(R.array.brand);
-        String[] bandArray = resources.getStringArray(R.array.band);
+        String[] categoriesArray = resources.getStringArray(R.array.categories);
 
         Random random = new Random();
-        int randomIndex = random.nextInt(12);
-        switch (randomIndex) {
-            case 0:
-                return new ArrayInfo(fruitArray, "fruit");
-            case 1:
-                return new ArrayInfo(animalArray, "animal");
-            case 2:
-                return new ArrayInfo(colorArray, "color");
-            case 3:
-                return new ArrayInfo(instrumentArray, "instrument");
-            case 4:
-                return new ArrayInfo(sportArray, "sport");
-            case 5:
-                return new ArrayInfo(footballArray, "club");
-            case 6:
-                return new ArrayInfo(countryArray, "country");
-            case 7:
-                return new ArrayInfo(foodArray, "food");
-            case 8:
-                return new ArrayInfo(vegetableArray, "vegetable");
-            case 9:
-                return new ArrayInfo(movieArray, "movie");
-            case 10:
-                return new ArrayInfo(brandArray, "brand");
-            case 11:
-                return new ArrayInfo(bandArray, "band");
-            default:
-                return new ArrayInfo(countryArray, "country");
-        }
+        int randomIndex = random.nextInt(categoriesArray.length);
+        String randomCategory = categoriesArray[randomIndex];
+        int arrayResourceId = context.getResources().getIdentifier(randomCategory, "array", context.getPackageName());
+        String[] currentArray = context.getResources().getStringArray(arrayResourceId);
+
+        return new ArrayInfo(currentArray, randomCategory);
     }
 
     private static class ArrayInfo {
